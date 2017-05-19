e8463c6c0f0988d6c5cde9f1963463314a785522
Нурсултан Алмаханов <nurik-1998kz@outlook.com>
2017-05-20 00:42:52 +0600

    end

--- FileExplorer/Form1.cs
+++ FileExplorer/Form1.cs
@@ -13,6 +13,9 @@ namespace FileExplorer
 {
     public partial class Form1 : Form
     {
+
+        Stack<string> st = new Stack<string>();
+
         State state = new State
         {
             Dir = new DirectoryInfo(@"D:\"),
@@ -32,7 +35,7 @@ namespace FileExplorer
             FileSystemInfo[] infos = state.Dir.GetFileSystemInfos();
             for (int i = 0; i < state.maxIndex; i++)
             {
-                listView1.Items.Add((i + 1) + (") ")+ infos[i].Name);
+                listView1.Items.Add(infos[i].FullName);
             }
         }
 
@@ -44,6 +47,59 @@ namespace FileExplorer
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
             
+        }
+
+        private void listView1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)(Keys.Back))
+            {
+                st.Pop();
+                int count = 0;   
+                
+                if (st.Count == 0)
+                {
+                    st.Push(@"D:\");
+                }
+                for (int i = 0; i < count; i++)
+                {
+                    listView1.Items[0].Remove();
+                }
+                listView1.Clear();
+                DirectoryInfo direct = new DirectoryInfo(@"" + st.Peek());
+                FileSystemInfo[] x = direct.GetFileSystemInfos();
+                
+                for (int i = 0; i < x.Length; i++)
+                {
+                    listView1.Items.Add(x[i].FullName);
+                }
+            }
+
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                int count = 0;     
+                for(int i = 0; i < listView1.Items.Count; i++)
+                {
+                    if (listView1.Items[i].Selected == true)
+                    {
+                        count = i;
+                    }
+                }
+
+                string path = listView1.Items[count].Text;
+                st.Push(path);
+                for (int i = 0; i < count; i++)
+                {
+                    listView1.Items[0].Remove();
+                }
+                listView1.Clear();
+                DirectoryInfo direct = new DirectoryInfo(@"" + path);
+                FileSystemInfo[] x = direct.GetFileSystemInfos();
+
+                for (int i = 0; i < x.Length; i++)
+                {
+                    listView1.Items.Add(x[i].FullName);
+                }
+            }
         }
     }
 }
